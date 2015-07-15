ac0b3c95f5508b34d91a39f80cafbef7a75220e9
kimihito <tatsurotamashiro@gmail.com>
2015-07-15 22:41:10 +0900

    修正

---
+++ install.sh
@@ -0,0 +1,10 @@
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew doctor
+brew update
+
+brew install python
+brew install ansible
+
+HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook -i hosts -vv localhost.yml
